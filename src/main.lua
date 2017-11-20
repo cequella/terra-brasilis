9dcfe5bb74ada9e218ed8b69cfeedc71858c5c69
cequella <pedro.oliveira.demolay@gmail.com>
2017-11-19 23:06:43 -0300

    Testing boarding/piemenu generation

--- src/main.lua
+++ src/main.lua
@@ -1,13 +1,34 @@
-require "MainMenuScene"
-require "InGameScene"
+world     = require "ecs.World"
+Component = require "ecs.Component"
+System    = require "ecs.System"
 
+require "CommonComponents"
+require "CommonSystems"
+require "CommonEntities"
+require "utils.Utils"
+
+function love.keypressed(key)
+   if key=="escape" then
+	  love.event.quit()
+   end
+
+   if key=="space" then
+   end
+end
+function love.keyreleased(key)
+end
 function love.load()
-   love.window.setTitle("Terra Brasilis")
-   
-   currentScene = InGameScene()
+   world
+	  :register( roundHighlight() )
+   	  :register( render() )
+	  :register( callActionMenu() )
+	  :register( pieMenuManager() )
+
+   board = Board(100, 100, 6, 6)
+end
+function love.update(dt)
+   world:update(dt)
 end
 function love.draw()
-   
-   currentScene:display()
-   
+   world:draw()
 end
